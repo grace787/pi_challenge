db92ae359879f442a2897987465f035bb75ea070
grace787 <155405926+grace787@users.noreply.github.com>
2024-01-15 21:49:33 -0800

    Update algorithm.sh
    improve calculation of PI

--- algorithm.sh
+++ algorithm.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
 
+echo "MODIFY THE ALGORITHM TO PRODUCE A MORE PRECISE CALCULATION OF PI" | tee report.txt
+echo "3.14159" | tee -a report
+
+echo "scale=1000; 4*a(1)" | bc -l |  tee report.txt
